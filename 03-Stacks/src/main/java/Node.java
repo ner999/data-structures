b849132f085e18b05f6ba8b8028e1d17ab2efce0
Ner <Nerijuscep@gmail.com>
2022-03-05 12:22:10 -0600

    Custom stack

---
+++ 03-Stacks/src/main/java/Node.java
@@ -0,0 +1,25 @@
+public class Node<T> {
+    public Node(T value) {
+        this.value = value;
+    }
+
+    private T value;
+    private Node next;
+
+    public T getValue() {
+        return value;
+    }
+
+    public void setValue(T value) {
+        this.value = value;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+//add constructors , getters and setters
+}
