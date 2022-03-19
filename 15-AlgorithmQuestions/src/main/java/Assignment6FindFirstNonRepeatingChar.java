025a837bd8e01768030083e8a9a05be50211bc24
Ner <Nerijuscep@gmail.com>
2022-03-19 12:17:23 -0500

    find first non repeating

---
+++ 15-AlgorithmQuestions/src/main/java/Assignment6FindFirstNonRepeatingChar.java
@@ -0,0 +1,26 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Assignment6FindFirstNonRepeatingChar {
+    public static void main(String[] args) {
+        System.out.println(findFirstNonRepeatingChar("a green apple"));
+    }
+    public static char findFirstNonRepeatingChar(String str){
+        Map<Character,Integer> map=new HashMap<>();
+        int count=0;
+        var chars=str.toCharArray();
+        for(Character ch: chars) {
+            if (map.containsKey(ch)) {
+                count=map.get(ch);
+                map.put(ch, count+1);
+            }else {
+                map.put(ch,1);
+            }
+
+        }for(Character ch: chars){
+            if (map.get(ch)==1) return ch;
+        }
+        return Character.MIN_VALUE;
+    }
+
+}
